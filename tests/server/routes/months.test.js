e758610deb802a445b6cc0715d871e6dee0444b4
Rebecca Jackson <zimbecs@gmail.com>
2019-03-11 17:11:16 +1300

    months routes tested

--- tests/server/routes/months.test.js
+++ tests/server/routes/months.test.js
@@ -4,7 +4,8 @@ const db = require('../../../server/db/db')
 const server = require('../../../server/server')
 
 jest.mock('../../../server/db/db', () => ({
-  getMonths: jest.fn()
+  getMonths: jest.fn(),
+  getMonthVeges: jest.fn()
 }))
 
 
@@ -27,3 +28,22 @@ test('GET /months loads array of object months', () => {
     expect(actual).toEqual(expected)
   })
 })
+
+test('POST /months loads array of vege objects', () => {
+  let expected = [{
+    0: {id: 1, name: 'Tomato', harvest: '8-17 weeks'},
+    1: {id: 2, name: 'Snow Pea', harvest: '12-14 weeks'},
+  }]
+
+  db.getMonthVeges.mockImplementation(() => Promise.resolve(expected))
+
+  return request(server).post('/api/months', 1)
+  .expect(200)
+  .then(res => {
+    let actual = res.body
+
+    expect(actual).toEqual(expected)
+  })
+})
+
+
